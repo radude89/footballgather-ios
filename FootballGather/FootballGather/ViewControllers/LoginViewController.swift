29214ae289623074bbc890147dcc843c23561bfb
Dan, Radu-Ionut (RO - Bucharest) <rdan@deloittece.com>
2019-06-06 18:04:14 +0300

    Mockups for view controllers

---
+++ FootballGather/FootballGather/ViewControllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  FootballGather
+//
+//  Created by Dan, Radu-Ionut (RO - Bucharest) on 06/06/2019.
+//  Copyright © 2019 Radu Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    // MARK: - Outlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var rememberMeSwitch: UISwitch!
+    
+    // MARK: - Actions
+    @IBAction func onLoginAction(_ sender: Any) {
+        //TODO
+    }
+    
+    @IBAction func onUseOfflineAction(_ sender: Any) {
+        
+    }
+    
+}
---
+++ FootballGather/FootballGather/ViewControllers/PlayerListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerListViewController.swift
+//  FootballGather
+//
+//  Created by Dan, Radu-Ionut (RO - Bucharest) on 06/06/2019.
+//  Copyright © 2019 Radu Dan. All rights reserved.
+//
+
+import UIKit
+
+class PlayerListViewController: UIViewController {
+
+}

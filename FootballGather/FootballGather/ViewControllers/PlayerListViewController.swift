b5cc1663a7babb92b0875ab8b021851e80286d57
Dan, Radu-Ionut (RO - Bucharest) <rdan@deloittece.com>
2019-06-24 16:11:45 +0300

    Implemented base player selection

--- FootballGather/FootballGather/ViewControllers/PlayerListViewController.swift
+++ FootballGather/FootballGather/ViewControllers/PlayerListViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class PlayerListViewController: UIViewController {
     
-    // MARK: - Properties
+    // MARK: - Outlets
+    
     @IBOutlet weak var playerTableView: UITableView!
+    @IBOutlet weak var bottomActionView: UIView!
+    @IBOutlet weak var bottomActionViewHeightConstraint: NSLayoutConstraint!
     
     lazy var loadingView = LoadingView.initToView(self.view)
     lazy var emptyView: EmptyView = {
@@ -20,14 +23,66 @@ class PlayerListViewController: UIViewController {
         return emptyView
     }()
     
+    private lazy var barButtonItem: UIBarButtonItem = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(selectPlayers))
+    
+    // MARK: - Variables
+    
     private let playersService = StandardNetworkService(resourcePath: "/api/players", authenticated: true)
     private var players: [PlayerResponseModel] = []
+    
+    private enum ViewState {
+        case list
+        case selection
+    }
+    private var viewState: ViewState = .list
+    
+    private var selectedPlayersDictionary: [Int: PlayerResponseModel] = [:]
+    
+    private enum ViewConstants {
+        static let bottomViewHeight: CGFloat = 80.0
+    }
+    
+    // MARK: - View life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bottomActionViewHeightConstraint.constant = 0
+        bottomActionView.isHidden = true
+        
+        title = "Players"
+        navigationItem.rightBarButtonItem = barButtonItem
         playerTableView.tableFooterView = UIView()
+        
         loadPlayers()
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func selectPlayers(sender: UIBarButtonItem) {
+        // toggle
+        title = "Players"
+        viewState = viewState == .list ? .selection : .list
+        
+        if viewState == .list {
+            sender.title = "Select"
+            bottomActionViewHeightConstraint.constant = 0
+            bottomActionView.isHidden = true
+        } else {
+            sender.title = "Cancel"
+            bottomActionViewHeightConstraint.constant = ViewConstants.bottomViewHeight
+            bottomActionView.isHidden = false
+        }
+        
+        playerTableView.reloadData()
+    }
+    
+    @IBAction func startGatherAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func deletePlayerAction(_ sender: Any) {
+        
     }
     
     private func loadPlayers() {
@@ -76,6 +131,13 @@ extension PlayerListViewController: UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         
+        if viewState == .list {
+            selectedPlayersDictionary[indexPath.row] = nil
+            cell.setupDefaultView()
+        } else {
+            cell.setupSelectionView()
+        }
+        
         let player = players[indexPath.row]
         cell.nameLabel.text = player.name
         
@@ -91,7 +153,31 @@ extension PlayerListViewController: UITableViewDelegate, UITableViewDataSource {
             cell.skillLabel.text = "Skill: -"
         }
         
+        cell.playerIsSelected = selectedPlayersDictionary[indexPath.row] != nil
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if viewState == .list {
+            // TODO: go to next screen
+        } else {
+            guard let cell: PlayerTableViewCell = tableView.cellForRow(at: indexPath) as? PlayerTableViewCell else {
+                return
+            }
+            
+            cell.playerIsSelected = !cell.playerIsSelected
+            
+            if cell.playerIsSelected {
+                selectedPlayersDictionary[indexPath.row] = players[indexPath.row]
+            } else {
+                selectedPlayersDictionary[indexPath.row] = nil
+            }
+            
+            let selectedPlayers = selectedPlayersDictionary.values.count
+            title = selectedPlayers > 0 ? "\(selectedPlayers) selected" : "Players"
+            
+        }
     }
 }
 

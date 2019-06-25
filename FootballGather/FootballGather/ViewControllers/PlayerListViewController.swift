597a3f2dfbe530a3d6f0890ab148164dcad469fc
Dan, Radu-Ionut (RO - Bucharest) <rdan@deloittece.com>
2019-06-25 22:11:29 +0300

    Finished selection and deletion

--- FootballGather/FootballGather/ViewControllers/PlayerListViewController.swift
+++ FootballGather/FootballGather/ViewControllers/PlayerListViewController.swift
@@ -81,12 +81,8 @@ class PlayerListViewController: UIViewController {
         
     }
     
-    @IBAction func deletePlayerAction(_ sender: Any) {
-        
-    }
-    
     private func loadPlayers() {
-        showLoadingView()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
         
         playersService.get { [weak self] (result: Result<[PlayerResponseModel], Error>) in
             guard let self = self else { return }
@@ -99,7 +95,7 @@ class PlayerListViewController: UIViewController {
                 }
             case .success(let players):
                 DispatchQueue.main.async {
-                    self.hideLoadingView()
+                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
                     
                     if players.isEmpty {
                         self.showEmptyView()
@@ -109,6 +105,27 @@ class PlayerListViewController: UIViewController {
 
                     self.players = players
                     self.playerTableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    private func deletePlayer(_ player: PlayerResponseModel, completion: @escaping (Bool) -> Void) {
+        var service = playersService
+        service.delete(withID: ResourceID.integer(player.id)) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.hideLoadingView()
+                    AlertHelper.present(in: self, title: "Error", message: String(describing: error))
+                    completion(false)
+                }
+            case .success(_):
+                DispatchQueue.main.async {
+                    self.hideLoadingView()
+                    completion(true)
                 }
             }
         }
@@ -176,8 +193,45 @@ extension PlayerListViewController: UITableViewDelegate, UITableViewDataSource {
             
             let selectedPlayers = selectedPlayersDictionary.values.count
             title = selectedPlayers > 0 ? "\(selectedPlayers) selected" : "Players"
-            
         }
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if viewState == .selection {
+            return false
+        }
+        
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let alertController = UIAlertController(title: "Delete player", message: "Are you sure you want to delete the selected player?", preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            guard let self = self else { return }
+            
+            let player = self.players[indexPath.row]
+            self.deletePlayer(player, completion: { [weak self] result in
+                guard result, let self = self else { return }
+                
+                tableView.beginUpdates()
+                self.selectedPlayersDictionary[indexPath.row] = nil
+                self.players.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                tableView.endUpdates()
+                
+                if self.players.isEmpty {
+                    self.showEmptyView()
+                }
+            })
+        }
+        alertController.addAction(confirmAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
 
